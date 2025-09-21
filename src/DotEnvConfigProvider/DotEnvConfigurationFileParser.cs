8f7fd22006aea73c0fc0721667860c9883e515fd
Jared Bartlett-Wright <jared.wbw@gmail.com>
2025-09-21 15:03:55 +1200

    Add basic handling for comments

--- src/DotEnvConfigProvider/DotEnvConfigurationFileParser.cs
+++ src/DotEnvConfigProvider/DotEnvConfigurationFileParser.cs
@@ -7,14 +7,20 @@ internal static class DotEnvConfigurationFileParser
         using TextReader reader = new StreamReader(input);
 
         var data = new Dictionary<string, string?>(StringComparer.OrdinalIgnoreCase);
+        
         var line = reader.ReadLine();
         while (line is not null)
         {
             try
             {
-                var equalsIndex = line.IndexOf('=');
-                var key = line.Substring(0, equalsIndex);
-                var value = line.Substring(equalsIndex + 1, line.Length - equalsIndex - 1);
+                var commentStartIndex = line.IndexOf('#');
+                var configLength = commentStartIndex == -1 ? line.Length : commentStartIndex;
+                
+                var config = line.AsSpan(0, configLength);
+                
+                var equalsIndex = config.IndexOf('=');
+                var key = config.Slice(0, equalsIndex).ToString();
+                var value = config.Slice(equalsIndex + 1, config.Length - equalsIndex - 1).ToString();
                 
                 data.Add(key, value);
             }
--- tests/DotEnvConfigProvider.Integration.Tests/DotEnvConfigProviderTests.cs
+++ tests/DotEnvConfigProvider.Integration.Tests/DotEnvConfigProviderTests.cs
@@ -24,6 +24,29 @@ public class DotEnvConfigProviderTests
         Assert.True(youGoofed);
     }
 
+    [Fact]
+    public void ShouldNotReadCommentsFromDotEnvConfig()
+    {
+         var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddDotEnvFile(path: "comments.test.env", optional: false)
+            .Build();
+
+         var commentedOutConfig = config.GetValue<string>("#commentedOutConfig");
+         var alsoCommentedOutConfig = config.GetValue<string>("# alsoCommentedOut");
+         var commentedOutWithWhiteSpace = config.GetValue<string>(" #commentedOutWithWhiteSpace");
+         
+         var configWithComment = config.GetValue<string>("configWithComment");
+         var configWithCommentAndWhiteSpace = config.GetValue<string>("configWithCommentAndWhiteSpace");
+         
+         Assert.Null(commentedOutConfig);
+         Assert.Null(alsoCommentedOutConfig);
+         Assert.Null(commentedOutWithWhiteSpace);
+         
+         Assert.Equal("big comment", configWithComment);
+         Assert.Equal("bigger comment ", configWithCommentAndWhiteSpace);
+    }
+
     public enum Foo
     {
         Baz,
